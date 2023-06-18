7302370400d7e82a402dd56ac6ef9c06dc4b2ddd
ALEXANDRE.SAMPAIO <allexonrails@gmail.com>
2023-06-18 15:36:27 -0300

    Adicionando Base Relative Root URL

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+ENV['RAILS_RELATIVE_URL_ROOT'] ||= '/api-suporte.me'
+BASE_ROOT_URL = ENV['RAILS_RELATIVE_URL_ROOT']
+
 # Initialize the Rails application.
 Rails.application.initialize!
